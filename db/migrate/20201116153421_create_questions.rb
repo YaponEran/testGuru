601a352f1af4f8a7b94b182b1e38244d969735e1
YaponEran <erannodepp9.4@gmail.com>
2020-11-16 21:41:51 +0600

    changed some attrubutes in model

--- db/migrate/20201116153421_create_questions.rb
+++ db/migrate/20201116153421_create_questions.rb
@@ -2,8 +2,6 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.text :body
-      t.integer :level
-      t.integer :test_id
 
       t.timestamps
     end
--- db/migrate/20201116153906_create_answers.rb
+++ db/migrate/20201116153906_create_answers.rb
@@ -2,8 +2,6 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.text :body
-      t.boolean :result
-      t.integer :question_id
 
       t.timestamps
     end
