5f92978bdf41d0c9983babd1d8c5b1ab167be466
YaponEran <erannodepp9.4@gmail.com>
2020-11-16 12:49:34 +0600

    adding validation null from migration

---
+++ db/migrate/20201116064551_add_categories_name_null_constraint.rb
@@ -0,0 +1,5 @@
+class AddCategoriesNameNullConstraint < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null(:categories, :title, false)
+  end
+end
---
+++ db/migrate/20201116064747_add_questions_name_null_constraint.rb
@@ -0,0 +1,5 @@
+class AddQuestionsNameNullConstraint < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null(:questions, :body, false)
+  end
+end
