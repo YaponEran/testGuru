9cb93cb9eea80320538074c215b563a5b94a5f2c
YaponEran <erannodepp9.4@gmail.com>
2020-11-17 11:18:12 +0600

    deleting files for change migration

--- db/migrate/20201116160348_add_user_null.rb
+++
@@ -1,7 +0,0 @@
-class AddUserNull < ActiveRecord::Migration[6.0]
-  def change
-    change_column(:users, :username, false)
-    change_column(:users, :email, false)
-    change_column(:users, :password, false)
-  end
-end
