667e5430b5868f5754dda040d7740571fef022a5
YaponEran <erannodepp9.4@gmail.com>
2020-11-23 14:46:30 +0600

    Adding new files for fixing version dev_3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,2 @@
 class Test < ApplicationRecord
-  def self.names_by_category(name){
-    joins('JOIN categories ON categories.id = tests.category_id')
-    .where(categories: {title: name}).pluck(:title)
-  }
 end
