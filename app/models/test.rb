3dc12cc6b81bfd7cf7c1f51a0f6a5df1a02b1932
YaponEran <erannodepp9.4@gmail.com>
2020-11-30 14:24:44 +0600

    users and tests assositaions through user_tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+  has_many :users through: :user_tests
   def self.names_by_category(name)
     joins('JOIN categories ON categories.id = tests.category_id')
     .where(categories: {title: name}).pluck(:title)
