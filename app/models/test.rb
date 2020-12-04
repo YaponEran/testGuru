6bfd9bca68b2c78c7df048463d11d2999253edfc
YaponEran <erannodepp9.4@gmail.com>
2020-12-04 13:16:15 +0600

    stupid bug made by me  fixed

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User'
+  
   has_many :questions, dependent: :destroy
-  has_many :authors, class_name: 'User'
 
   has_many :user_tests, dependent: :destroy
   has_many :users, through: :user_tests
