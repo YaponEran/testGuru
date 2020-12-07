5972feb68ad4dffaa83528538b3a330b35784bf3
YaponEran <erannodepp9.4@gmail.com>
2020-12-08 00:06:53 +0600

    answer and test scopes added

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,15 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
-  
+
   has_many :questions, dependent: :destroy
 
   has_many :user_tests, dependent: :destroy
   has_many :users, through: :user_tests
+
+  scope :easy, -> {where(level: 0..1)}
+  scope :medium, -> {where(level: 2..4)}
+  scope :hard, -> {where(level: 5..Float::INFINITY)}
 
   def self.names_by_category(name)
     joins('JOIN categories ON categories.id = tests.category_id')
