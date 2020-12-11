d556b87639ef5072f049f528a7e0d6e5527597a5
YaponEran <erannodepp9.4@gmail.com>
2020-12-12 00:56:17 +0600

    added option to answer validation size

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ApplicationRecord
   scope :correct_answer, -> { where(correct: true) }
 
   validates :body, presence: true
-  validate :validate_answer_size
+  validate :validate_answer_size, on: :create
 
   private
   def validate_answer_size
