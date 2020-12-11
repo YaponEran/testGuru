4751f6df911dfc03a21c6395ba6f08aee5f7e63b
YaponEran <erannodepp9.4@gmail.com>
2020-12-12 00:44:22 +0600

    syntax bus fixed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
-  has_many :tests, dependent: :destroy
+  default_scope { order(title: :asc) }
 
-  default_scope {order(title: 'ASC')}
+  has_many :tests, dependent: :destroy
 
   validates :title, presence: true
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -7,15 +7,15 @@ class Test < ApplicationRecord
   has_many :user_tests, dependent: :destroy
   has_many :users, through: :user_tests
 
-  scope :easy, -> {where(level: 0..1)}
-  scope :medium, -> {where(level: 2..4)}
-  scope :hard, -> {where(level: 5..Float::INFINITY)}
-  scope :level, -> (level) {where(level: level)}
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :level, -> (level) { where(level: level) }
 
-  scope :by_category, -> (name) {joins(:category).where(categories: {title: name})}
+  scope :by_category, -> (name) { joins(:category).where(categories: {title: name}) }
 
-  validates :title, presence: true, uniqueness: {scope: [:title, :level]}
-  validates :level, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   # def self.names_by_category(name)
   #   joins('JOIN categories ON categories.id = tests.category_id')
