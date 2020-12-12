f9f7c7a6fdc69bc208b07c8545073fc92f68708b
YaponEran <erannodepp9.4@gmail.com>
2020-12-12 15:57:52 +0600

    Question actions created

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,44 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_form ActiveRecord::RecordNotFound, with: :rescue_question_not_found
+
+  def index
+    @questions = Question.all
+    render json: @questions
+  end
+
+  def show
+    render inline: "<li> <%= @question.body %> </li>"
+  end
+
+  def new
+  end
+
+  def create
+    if @test.questions.create(question_params)
+      redirect_to test_question_path
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+  
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_question_not_found
+    render plain: "Question not found"
+  end
 end
