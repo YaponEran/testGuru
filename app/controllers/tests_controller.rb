4ee3ace2a8154d4549d597552f034b1eeb003dcc
YaponEran <erannodepp9.4@gmail.com>
2020-12-20 17:21:12 +0600

    Test new and form parila created

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,50 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit destroy update]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @test
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @test
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @test.destroy 
+    redirect_to tests_path
+  end
+
+  private
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
 end
