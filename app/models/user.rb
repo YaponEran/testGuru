05b8adaec72dfe6ea4cdcc9a72759b3ec624f17a
YaponEran <erannodepp9.4@gmail.com>
2021-01-11 02:01:31 +0600

    added true assostians to user, test, test_passages and added test_pasge methot to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :user_tests, dependent: :destroy
-  has_many :tests, through: :user_tests
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
 
   has_many :own_tests, class_name: 'Test', foreign_key: :author_id
 
@@ -8,5 +8,9 @@ class User < ApplicationRecord
 
   def test_by_level(level)
     tests.level(level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
