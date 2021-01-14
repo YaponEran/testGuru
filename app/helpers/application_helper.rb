76cb568a7e7782e44d00b6a7b69b498b4fd885bc
YaponEran <erannodepp9.4@gmail.com>
2021-01-14 07:08:28 +0600

    flash message heleper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'Test-Guru', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_message(alert)
+    if flash[:alert]
+      content_tag :p, flash[:alert], class: 'flash alert'
+    end
+  end
+  
 end
