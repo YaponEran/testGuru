6551be67a156f9a3536b8db7ab24f3787ffafb71
YaponEran <erannodepp9.4@gmail.com>
2021-01-22 02:03:13 +0600

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
-  before_action :configure_permited_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def after_sign_in_path_for(user)
